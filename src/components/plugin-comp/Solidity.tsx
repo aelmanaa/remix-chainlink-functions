a5f7e1cfc0f7d775b57e0f298974cf79113b54fa
aelmanaa <aelmanaa@gmail.com>
2023-02-04 17:11:42 +0100

    remix samples

--- src/components/plugin-comp/Solidity.tsx
+++ src/components/plugin-comp/Solidity.tsx
@@ -1,23 +1,33 @@
 import Col from "react-bootstrap/Col"
 import Form from "react-bootstrap/Form"
 import Row from "react-bootstrap/Row"
+import { useSelector } from "react-redux"
+import { RootState } from "../../redux/store"
 
 export const Solidity = () => {
+  const compiledSolidityFiles = useSelector((state: RootState) => state.remix.compiledSolidityFiles)
+  const solidityFiles = Object.keys(compiledSolidityFiles)
   const errorMessage = ""
   return (
     <div>
       <h2>Solidity</h2>
       <Form>
         <Row>
-          <Col>
-            <Form.Select defaultValue="dfdfg" />
-          </Col>
-          <Col>
-            <Form.Check disabled type="switch" checked={!errorMessage} />
-          </Col>
-          <Col>
-            <Form.Control plaintext readOnly defaultValue={errorMessage || "Compiled!"} />
-          </Col>
+          <Form.Select>
+            {solidityFiles.map((opt) => {
+              const contracts = compiledSolidityFiles[opt].contracts
+              return Object.keys(contracts).map((contract) => {
+                return (
+                  <option key={opt + "_" + contract} data-key={opt + "_" + contract}>
+                    {contract}
+                  </option>
+                )
+              })
+            })}
+          </Form.Select>
+        </Row>
+        <Row>
+          <Form.Control plaintext readOnly defaultValue={errorMessage || "Compiled!"} />
         </Row>
       </Form>
     </div>
